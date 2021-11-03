14a5b5e9d9238bf8879806efd0d6219c8fb1352e
JuaniVega <juanivega0206@gmail.com>
2021-11-03 00:34:11 -0300

    Ejercicio 2 casi terminado

--- app/src/main/java/com/example/tp3/crear_record.java
+++ app/src/main/java/com/example/tp3/crear_record.java
@@ -2,9 +2,14 @@ package com.example.tp3;
 
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +17,7 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class crear_record extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
 
@@ -21,6 +26,13 @@ public class crear_record extends AppCompatActivity implements TimePickerDialog.
     private Button btnGuardar;
     private TextView tvFecha;
     private TextView tvHora;
+    private android.support.design.widget.TextInputEditText tfNota;
+
+    int diaCal;
+    int mesCal;
+    int añoCal;
+    int horaCal;
+    int minCal;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +43,13 @@ public class crear_record extends AppCompatActivity implements TimePickerDialog.
         btnHora=findViewById(R.id.btnSelHora);
         tvFecha=findViewById(R.id.tvFecha);
         tvHora=findViewById(R.id.tvHora);
+        tfNota=findViewById(R.id.tfNota);
         int dia= Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         int mes= Calendar.getInstance().get(Calendar.MONTH);
         int año= Calendar.getInstance().get(Calendar.YEAR);
         int hora= Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         int min= Calendar.getInstance().get(Calendar.MINUTE);
+        RecordatorioReceiver rec = new RecordatorioReceiver();
 
         btnFecha.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +68,23 @@ public class crear_record extends AppCompatActivity implements TimePickerDialog.
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // final AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-              //  alarm.set(AlarmManager.RTC_WAKEUP, tiempoEnMillis, pendingIntentConActionParaMiBroadcastReceiver);
+                final AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                Intent intent= new Intent(crear_record.this, RecordatorioReceiver.class);
+                intent.putExtra("TEXTO", tfNota.getText().toString());
+                intent.setAction(rec.RECORDATORIO);
+                PendingIntent   pendingIntent=PendingIntent.getBroadcast(getApplicationContext(), 0, intent,0);
+                alarm.set(AlarmManager.RTC_WAKEUP, (fechaEnMillis(añoCal,mesCal,diaCal,horaCal,minCal)),  pendingIntent);
 
             }
         });
+    }
+
+    private long fechaEnMillis(int año, int mes, int dia, int hora, int min){
+        Calendar cal=Calendar.getInstance();
+        cal.set(año,mes,dia,hora,min);
+        long fechaAlarma=cal.getTimeInMillis();
+
+        return fechaAlarma;
     }
 
     private void mostrarPickerFecha(int dia, int mes, int año) {
@@ -73,6 +99,10 @@ public class crear_record extends AppCompatActivity implements TimePickerDialog.
 
     @Override
     public void onDateSet(DatePicker datePicker, int año, int mes, int dia) {
+        diaCal=dia;
+        mesCal=mes;
+        añoCal=año;
+
         mes = mes+1;
         String fecha= dia+" / "+mes+" / "+año;
         tvFecha.setText(fecha);
@@ -80,6 +110,9 @@ public class crear_record extends AppCompatActivity implements TimePickerDialog.
 
     @Override
     public void onTimeSet(TimePicker timePicker, int hora, int min) {
+        horaCal=hora;
+        minCal=min;
+
         String horario= hora+" : "+min;
         tvHora.setText(horario);
     }
